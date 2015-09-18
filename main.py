5cf369e5053c77de7ba5e35ab145cd114cd588a9
pyskell <ajlusardi@gmail.com>
2015-09-18 16:03:22 -0400

    starting off

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import praw
+import OAuth2Util
 from private_settings import *
 from settings import *
 
@@ -7,32 +8,18 @@ username   = "Charity-Bot"
 trigger    = "/u/Charity-Bot"
 
 r = praw.Reddit(user_agent)
-r.set_oauth_app_info(client_id=private_id,
-                     client_secret=private_secret,
-                     redirect_uri = redirect_uri)
-r.refresh_access_information(refresh_token)
-r.login(username, password)
+o = OAuth2Util.OAuth2Util(r)
 
-# Uncomment to test connection
-# authenticated_user = r.get_me()
-# print(authenticated_user.name)
+mentions_file = open('processed_mentions', 'a+')
 
-mentions = r.request_json('https://www.reddit.com/message/mentions/')
+mentions_file.seek(0)
+processed_mentions = mentions_file.read().splitlines()
+
+o.refresh()
+mentions = r.get_mentions()
 
 for mention in mentions:
-  print(mention)
-
-# TODO: Get fed-up and put this in another script...
-
-# Uncomment to get new access_code
-# url = r.get_authorize_url('uniqueKey', 'identity edit flair history privatemessages read save submit', True)
-# import webbrowser
-# webbrowser.open(url)
-
-# Uncomment to get new refresh_token after getting new access_code
-# access_information = r.get_access_information(access_code)
-# r.set_access_credentials(**access_information)
-# print(authenticated_user.name, access_information['refresh_token'])
-
-
-# r.send_message('pyskell', 'test', 'this is a test')
+  if mention.id not in processed_mentions:
+    print(mention)
+  else:
+    mentions_file.write(mention.id + '\n')
