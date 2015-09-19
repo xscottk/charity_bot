31409dbb5ce9fc70a313f3a336116306c7f5adf4
pyskell <ajlusardi@gmail.com>
2015-09-19 12:59:16 -0400

    going shoe shopping so i guess i should commit what i have

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+# REMINDER: In production make sure we use a different email and maybe name for accepting donations.
+
+# Example: r = requests.get('https://api-sandbox.justgiving.com/JUSTGIVING_APP_ID/v1/fundraising/pages', auth=HTTPBasicAuth('JUSTGIVING_USER', 'JUSTGIVING_PASS'), headers={'accept':'application/json'})
+
 import OAuth2Util
 import praw
 import requests
+from requests.auth import HTTPBasicAuth
 
 from private_settings import *
 from settings import *
 
-user_agent = "web:charity:v0.0.1 (by /u/pyskell)"
-username   = "Charity-Bot"
-trigger    = "/u/Charity-Bot"
-
-r = praw.Reddit(user_agent)
+r = praw.Reddit(REDDIT_USER_AGENT)
 o = OAuth2Util.OAuth2Util(r)
 
 mentions_file = open('processed_mentions', 'a+')
@@ -20,46 +21,46 @@ processed_mentions = mentions_file.read().splitlines()
 o.refresh()
 mentions = r.get_mentions()
 
-def get_valid_currencies():
-  # Get the list of valid currencies from justgiving
+def request_wrapper(resource, method, headers, **kwargs):
+  # Return either the requested resource (in JSON), POST confirmation, or None if something failed
+  # QUESTION: Should I do subclassing instead here?
+  # TODO: Figure out how to merge default headers and optional additional headers passed to request_wrapper
 
-  # TODO: For now we just return a list manually...may be nice to periodically check for new currencies in the future.
-  # return [('GBP','£') , ('USD','$') , ('HKD','HK$') , ('SGD','SG$') , ('CAD','CA$') , ('AED','د.إ') , ('AUD','AU$') , ('ZAR','R') , ('EUR','€')]
-  return ['£','$','HK$','SG$','CA$','د.إ','AU$','R','€']
+  if method == 'GET':
+    request = requests.get(JUSTGIVING_API_URL + resource, auth=HTTPBasicAuth(JUSTGIVING_USER, JUSTGIVING_PASS), headers={'accept':'application/json'}, **kwargs)
 
-def parse_donation(donation):
-  valid_currencies = get_valid_currencies()
-  
+  elif method == 'POST':
+    request = requests.post(JUSTGIVING_API_URL + resource, auth=HTTPBasicAuth(JUSTGIVING_USER, JUSTGIVING_PASS), headers={'accept':'application/json'}, **kwargs)
 
-def validate_charity_id(charity_id):
-  pass
-
-def parse_message(mention):
-  # Parse and validate donation message.
-
-  # Call format: \u\Charity-Bot $X Y ($5 123)
-  # Currency can be $ or pound or OTHERS?
-  # Default Charity: \u\Charity-Bot $X 2357 (Charity ID for Cancer Research UK)
-
-  # Rules:
-  # Currency sign must be valid
-  # Donation amount must be >0
-  # Charity must be a valid charity number
-  message = mention.body.split()
-  
-  # Make sure we get the right number of arguments (3): /u/Charity-Bot Amount Charity
-  if len(message) != 3:
+  else:
     return None
 
-  currency, donation_amount  = parse_donation(message[1])
-  charity_id                 = validate_charity_id(message[2])
+  return request
+
+def validate_charityId(mention):
+  # Parse and validate donation message.
+  # RETURN: Validated charity number OR None if invalid charity number.
+
+  # Call format: \u\Charity-Bot donate charityID
+  # Default Charity if no charity specified: \u\Charity-Bot donate 2357 (Charity ID for Cancer Research UK)
+
+  # Rules:
+  # First argument must be the word 'donate'
+  # Charity must be a valid charity number
+
+  message = mention.body.split()
+
+  donate  = message[0] == True
+  charity_id = message[1]
+
+  if not donate:
+    return None
 
 
 
-
-  donation = [donation_amount, charity_id]
-
-  return donation
+  # Make sure we get the right number of arguments (2 or 3) 
+  # if len(message) == 3:
+    
 
 def get_donation(donation_amount, charity):
   # Get the donation link from justgiving.
@@ -81,12 +82,12 @@ def init():
   for mention in mentions:
     if mention.id not in processed_mentions:
       # print(mention)
-      donation_details = parse_message(mention)
+      donation_details = validate_charityId(mention)
       print(donation_details)
-      get_donation()
-      send_donation_message()
-      confirm_donation() # Will need a way to repeatedly check this...may be better calling and then queuing up a checking system after sending the donation message
-      post_confirmation()
+      # get_donation()
+      # send_donation_message()
+      # confirm_donation() # Will need a way to repeatedly check this...may be better calling and then queuing up a checking system after sending the donation message
+      # post_confirmation()
 
       # TODO: Switch to sqlite at some point
       # TODO: REMEMBER TO UNCOMMENT THIS IN PRODUCTION
--- settings.py
+++ settings.py
@@ -1,1 +1,14 @@
-redirect_uri = 'http://127.0.0.1:65010/authorize_callback'
+from private_settings import *
+
+CONTENT_TYPE = 'application/json'
+
+REDDIT_USER_AGENT   = "web:charity:v0.0.1 (by /u/pyskell)"
+REDDIT_USERNAME     = "Charity-Bot"
+REDDIT_TRIGGER      = "/u/Charity-Bot"
+
+REDDIT_REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
+
+JUSTGIVING_BASE_URL = 'https://api-sandbox.justgiving.com' # http://v3-sandbox.justgiving.com is the corresponding website
+
+# QUESTION: Is there a better way to assemble a URL?
+JUSTGIVING_API_URL = JUSTGIVING_BASE_URL + '/' + JUSTGIVING_APP_ID + '/'
