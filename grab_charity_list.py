ad2bce37d0b245eda000963aa13c934a169f7e0b
pyskell <ajlusardi@gmail.com>
2015-09-30 13:48:21 -0400

    quick charity list grabber

--- grab_charity_list.py
+++ grab_charity_list.py
@@ -7,6 +7,7 @@ from requests.auth import HTTPBasicAuth
 charity_list = open('charity_list.json', 'w')
 json_dict = {}
 
+#Quick, dirty, not future proof
 for i in range(1,113):
     list = requests.get('https://api.justgiving.com/0076b959/v1/charity/search?pagesize=100&page=' + str(i), auth=HTTPBasicAuth('ajlusardi@gmail.com', 'b1bbd510-62ea-11e5-b115'), headers={'accept':'application/json'})
     json_dict.update(list.json())
