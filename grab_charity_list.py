07e3e56b021856f8750f9f6ddf7a4a3ac44187a3
pyskell <ajlusardi@gmail.com>
2015-10-01 12:36:00 -0400

    pulled charity list

--- grab_charity_list.py
+++ grab_charity_list.py
@@ -5,12 +5,17 @@ import time
 from requests.auth import HTTPBasicAuth
 
 charity_list = open('charity_list.json', 'w')
-json_dict = {}
+json_list = []
 
 #Quick, dirty, not future proof
 for i in range(1,113):
+  try:
+    print("Getting page number:", i)
     list = requests.get('https://api.justgiving.com/0076b959/v1/charity/search?pagesize=100&page=' + str(i), auth=HTTPBasicAuth('ajlusardi@gmail.com', 'b1bbd510-62ea-11e5-b115'), headers={'accept':'application/json'})
-    json_dict.update(list.json())
-    time.sleep(20)
+    json_list.append(list.json())
+    time.sleep(10)
+  except:
+    # If we hit an error, dump what we have
+    json.dump(json_list, charity_list)
 
-json.dump(json_dict, charity_list)
+json.dump(json_list, charity_list)
